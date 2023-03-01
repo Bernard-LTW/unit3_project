0af94ccd2409790d0a870353a27ae35a06f7ac2c
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2023-03-01 23:08:18 +0900

    everything sort of works so yay

--- Code/VocabAppMain.py
+++ Code/VocabAppMain.py
@@ -1,14 +1,19 @@
-#VocabAppMain.py
+# VocabAppMain.py
+from kivy.logger import Logger
 from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.screen import MDScreen
-from secure_password import hash_password
+
 from db_manager import database_handler
-from models import Users, Vocabulary, UserStats
-from kivy.logger import Logger
+from models import Users, Vocabulary
+from secure_password import hash_password
 
 global current_user
 current_user = None
+global vocab_list
+vocab_list = []
+
 
 class LoginScreen(MDScreen):
     def login(self):
@@ -26,11 +31,11 @@ class LoginScreen(MDScreen):
         else:
             print("Login failed")
 
-class RegisterScreen(MDScreen):
 
+class RegisterScreen(MDScreen):
     def register(self):
         print(f"Username: {self.ids.uname.text} Email:{self.ids.email.text} Password: {self.ids.pwd.text}")
-        uname  = self.ids.uname.text
+        uname = self.ids.uname.text
         email = self.ids.email.text
         pwd = self.ids.pwd.text
         cpwd = self.ids.cpwd.text
@@ -52,18 +57,22 @@ class LandingScreen(MDScreen):
     def on_enter(self):
         global current_user
         self.ids.welcome_banner.text = f"こんにちは {current_user.username}"
+
     def logout(self):
         self.parent.current = "LoginScreen"
         current_user = None
+
     def add_vocab(self):
         self.parent.current = "AddVocabScreen"
+
 
 class ManageVocabScreen(MDScreen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         font_name = "Roboto"
+        self.checked_rows = None
         self.data_table = MDDataTable(
-            size_hint=(0.9, 0.5),
+            size_hint=(0.9, 0.7),
             pos_hint={"center_x": 0.5, "center_y": 0.5},
             use_pagination=True,
             check=True,
@@ -91,53 +100,217 @@ class ManageVocabScreen(MDScreen):
         print(f"Row {current_row} was checked")
 
     def load(self):
-        print("Trying to load all Tx...")
+        Logger.info("Loading Vocabulary from database...")
         self.data_table.row_data.clear()
         rows = VocabApp.db.get_vocab()
-        print(self.data_table.row_data)
-        for row in rows:
-            print(row)
-            print(row.id)
-            row.hiragana = f"[font=Japanese.ttc]{row.hiragana}[/font]"
-            row.katakana = f"[font=Japanese.ttc]{row.katakana}[/font]"
-            row = [str(row.id), str(row.lesson), str(row.part_of_lesson), row.hiragana, row.katakana, row.definition]
-            print(row)
-            if row not in self.data_table.row_data:
-                self.data_table.row_data.append(row)
+        try:
+            for row in rows:
+                temp_hiragana = f"[font=Japanese.ttc]{row.hiragana}[/font]"
+                temp_katakana = f"[font=Japanese.ttc]{row.katakana}[/font]"
+                row = [str(row.id), str(row.lesson), str(row.part_of_lesson), temp_hiragana, temp_katakana,
+                       row.definition]
+                if row not in self.data_table.row_data:
+                    self.data_table.row_data.append(row)
+        except Exception as e:
+            Logger.error(f"Error loading vocabulary: {e}")
 
+    def delete_vocab(self):
+        checked = self.data_table.get_row_checks()
+        Logger.info("Deleting vocabulary from database...")
+        try:
+            for row in checked:
+                vocab_id = int(row[0])
+                VocabApp.db.delete_vocab(vocab_id=vocab_id)
+            self.load()
+        except Exception as e:
+            Logger.error(f"Error deleting vocabulary: {e}")
+
+    def enter_edit(self):
+        checked = self.data_table.get_row_checks()
+        if len(checked) == 1:
+            vocab_id = int(checked[0][0])
+            vocab = VocabApp.db.get_vocab_by_id(vocab_id=vocab_id)
+            self.parent.get_screen("PerVocabManageScreen").ids.selected_lesson.text = str(vocab.lesson)
+            self.parent.get_screen("PerVocabManageScreen").ids.selected_part.text = str(vocab.part_of_lesson)
+            self.parent.get_screen("PerVocabManageScreen").ids.selected_hiragana.text = vocab.hiragana
+            self.parent.get_screen("PerVocabManageScreen").ids.selected_katakana.text = vocab.katakana
+            self.parent.get_screen("PerVocabManageScreen").ids.selected_english.text = vocab.definition
+            self.parent.current = "PerVocabManageScreen"
+
+        else:
+            Logger.error("Please select only one vocabulary to edit")
+            dialog = MDDialog(
+                title="Warning",
+                text="Please select only one vocabulary to edit",
+                size_hint=(0.7, 0.3)
+            )
+            dialog.open()
 
 
 class PerVocabManageScreen(MDScreen):
-    def add_vocab(self):
-        pass
-    def edit_vocab(self):
-        pass
+    def on_pre_enter(self, *args):
+        if self.ids.selected_hiragana.text == "":
+            self.ids.selected_save.on_press = self.add_vocab
+        else:
+            self.ids.selected_save.text = "Save Changes"
+            self.ids.selected_save.on_press = self.save_changes
 
-    def delete_vocab(self):
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.dialog = MDDialog(
+            title="Warning",
+            text="The vocabulary already exists in the database.",
+            size_hint=(0.7, 0.3)
+        )
+
+    def add_vocab(self):
+        Logger.info("Adding vocabulary to database...")
+        lesson = int(self.ids.selected_lesson.text)
+        part = int(self.ids.selected_part.text)
+        hiragana = self.ids.selected_hiragana.text
+        katakana = self.ids.selected_katakana.text
+        definition = self.ids.selected_english.text
+
+        try:
+            if VocabApp.db.session.query(Vocabulary).filter_by(hiragana=hiragana).first() is not None:
+                self.dialog.open()
+            else:
+                VocabApp.db.insert_vocab(lesson, part, hiragana, katakana, definition)
+                Logger.info("Vocabulary added successfully")
+                self.clear_fields()
+            self.parent.current = "ManageVocabScreen"
+        except Exception as e:
+            Logger.error(f"Error adding vocabulary: {e}")
+
+    def clear_fields(self):
+        self.ids.selected_lesson.text = ""
+        self.ids.selected_part.text = ""
+        self.ids.selected_hiragana.text = ""
+        self.ids.selected_katakana.text = ""
+        self.ids.selected_english.text = ""
+        self.ids.selected_save.text = "Save"
+        self.ids.selected_save.on_press = self.add_vocab
+
+    def save_changes(self):
+        Logger.info("Saving changes to vocabulary...")
+        lesson = int(self.ids.selected_lesson.text)
+        part = int(self.ids.selected_part.text)
+        hiragana = self.ids.selected_hiragana.text
+        katakana = self.ids.selected_katakana.text
+        definition = self.ids.selected_english.text
+        try:
+            VocabApp.db.update_vocab(lesson, part, hiragana, katakana, definition)
+            Logger.info("Vocabulary updated successfully")
+            self.clear_fields()
+            self.parent.current = "ManageVocabScreen"
+            ManageVocabScreen.load(self.parent.get_screen("ManageVocabScreen"))
+        except Exception as e:
+            Logger.error(f"Error updating vocabulary: {e}")
+
 
 class RandomVocabScreen(MDScreen):
     def random_vocab(self):
-        pass
+        global current_user
+        global vocab_list
+        global count
+        vocab_list = []
+        count = 0
+        Logger.info("Choosing random vocabulary...")
+        try:
+            vocab = VocabApp.db.get_vocab_by_user_stats(current_user.id)
+            for v in vocab:
+                temp = (v.hiragana, v.katakana, v.definition, v.id)
+                vocab_list.append(temp)
+            self.parent.get_screen("VocabCardScreen").ids.hiragana_label.text = vocab_list[count][0]
+            self.parent.get_screen("VocabCardScreen").ids.katakana_label.text = vocab_list[count][1]
+            self.parent.get_screen("VocabCardScreen").ids.vocab_description.text = vocab_list[count][2]
+            self.parent.current = "VocabCardScreen"
+        except Exception as e:
+            Logger.error(f"Error choosing random vocabulary: {e}")
 
 class VocabChooserScreen(MDScreen):
+    def clear_fields(self):
+        self.ids.selected_lesson.text = ""
+        self.ids.selected_part.text = ""
 
     def choose_vocab(self):
-        pass
+        Logger.info("Choosing vocabulary...")
+        lesson = int(self.ids.selected_lesson.text)
+        part = int(self.ids.selected_part.text)
+        global count
+        count = 0
+        global vocab_list
+        try:
+            vocabs = VocabApp.db.get_vocab_by_lesson_and_part(lesson, part)
+            for vocab in vocabs:
+                temp = (vocab.hiragana, vocab.katakana, vocab.definition, vocab.id)
+                print(temp)
+                vocab_list.append(temp)
+            self.parent.get_screen("VocabCardScreen").ids.hiragana_label.text = vocab_list[count][0]
+            self.parent.get_screen("VocabCardScreen").ids.katakana_label.text = vocab_list[count][1]
+            self.parent.get_screen("VocabCardScreen").ids.vocab_description.text = f"Definition: {vocab_list[count][2]}"
+            self.parent.current = "VocabCardScreen"
+            self.clear_fields()
+        except Exception as e:
+            Logger.error(f"Error choosing vocabulary: {e}")
+
 
 class VocabCardScreen(MDScreen):
-    def show_vocab(self):
-        pass
-
+    def next_vocab(self):
+        print("Next vocab")
+        global count
+        global vocab_list
+        if count > len(vocab_list)-1:
+            self.ids.vocab_description.text = "You have reached the end of the list"
+            self.ids.katakana_label.text = ""
+            self.ids.hiragana_label.text = ""
+        else:
+            self.parent.get_screen("VocabCardScreen").ids.hiragana_label.text = vocab_list[count][0]
+            self.parent.get_screen("VocabCardScreen").ids.katakana_label.text = vocab_list[count][1]
+            self.parent.get_screen("VocabCardScreen").ids.vocab_description.text = f"Definition: {vocab_list[count][2]}"
+    def toggle_japanese(self):
+        #print(self.ids.hiragana_label.color)
+        if self.ids.hiragana_label.color == [1, 1, 1, 1]:
+            self.ids.show_japanese.text = "Hide Japanese"
+            self.ids.hiragana_label.color = [0, 0, 0, 1]
+            self.ids.katakana_label.color = [0, 0, 0, 1]
+        else:
+            self.ids.show_japanese.text = "Show Japanese"
+            self.ids.hiragana_label.color = [1, 1, 1, 1]
+            self.ids.katakana_label.color = [1, 1, 1, 1]
     def add_points(self):
-        pass
+        global count
+        global current_user
+        try:
+            user_id = current_user.id
+            id = vocab_list[count][3]
+            VocabApp.db.update_user_stats(user_id, id, 1)
+            Logger.info("Points removed successfully")
+            count += 1
+            self.next_vocab()
+        except Exception as e:
+            Logger.error(f"Error removing points: {e}")
 
     def remove_points(self):
-        pass
+        global count
+        global current_user
+        try:
+            user_id = current_user.id
+            id = vocab_list[count][3]
+            VocabApp.db.update_user_stats(user_id, id, -1)
+            Logger.info("Points removed successfully")
+            count += 1
+            self.next_vocab()
+        except Exception as e:
+            Logger.error(f"Error removing points: {e}")
+
+
 class VocabApp(MDApp):
     db = database_handler("vocab_app.db")
+
     def build(self):
         return
+
 
 boi = VocabApp()
 boi.run()
--- Code/db_manager.py
+++ Code/db_manager.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from models import Users, Base, Vocabulary, UserStats
 from secure_password import check_password
-
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 def create_base():
     engine = create_engine('sqlite:///vocab_app.db')
@@ -50,8 +51,64 @@ class database_handler:
         vocab = self.session.query(Vocabulary).all()
         return vocab
 
+    def insert_vocab(self, lesson, part, hiragana, katakana, definition):
+        if self.session.query(Vocabulary).filter_by(hiragana=hiragana).first() is not None:
+            print("Vocab already exists")
+            return False
+        else:
+            new_vocab = Vocabulary(lesson=lesson, part_of_lesson=part, hiragana=hiragana, katakana=katakana, definition=definition)
+            self.session.add(new_vocab)
+            self.session.commit()
+            print("Vocab added")
+            return None
+
+    def update_vocab(self, lesson, part, hiragana, katakana, definition):
+        vocab = self.session.query(Vocabulary).filter_by(hiragana=hiragana).first()
+        vocab.lesson = lesson
+        vocab.part = part
+        vocab.hiragana = hiragana
+        vocab.katakana = katakana
+        vocab.definition = definition
+        self.session.commit()
+        return None
+
+    def delete_vocab(self, vocab_id):
+        vocab = self.session.query(Vocabulary).filter_by(id=vocab_id).first()
+        self.session.delete(vocab)
+        self.session.commit()
+        return None
+    def get_vocab_by_id(self,vocab_id):
+        if self.session.query(Vocabulary).filter_by(id=vocab_id).first() is None:
+            return None
+        else:
+           return self.session.query(Vocabulary).filter_by(id=vocab_id).first()
+
+    def get_vocab_by_lesson_and_part(self, lesson, part):
+        vocab = self.session.query(Vocabulary).filter_by(lesson=lesson, part_of_lesson=part).all()
+        return vocab
+
+    def update_user_stats(self, user_id, vocab_id, point_change):
+        user_stats = self.session.query(UserStats).filter_by(user_id=user_id, vocabulary_id=vocab_id).first()
+        print(f"User stats: {user_stats}")
+        if user_stats is None:
+            new_user_stats = UserStats(user_id=user_id, vocabulary_id=vocab_id, points=100)
+            new_user_stats.points += point_change
+            self.session.add(new_user_stats)
+            self.session.commit()
+        else:
+            user_stats.points += point_change
+            self.session.commit()
+        return None
+
+    def get_vocab_by_user_stats(self, user_id):
+    #get all vocab from userstats where user_id = user_id sort by points ascending
+        vocab = self.session.query(Vocabulary).join(UserStats).filter(UserStats.user_id == user_id).order_by(UserStats.points).all()
+        return vocab
+
+
 create_base()
 test = database_handler("vocab_app.db")
 print(test.check_user("haha@haha.com"))
+
 
 
